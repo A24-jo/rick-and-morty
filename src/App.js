28732dd5dfee6624b48799cac8cb55a890a2830b
A24-jo <bravo24ramosjose@gmail.com>
2023-06-28 19:31:09 -0500

    jo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Buscador from './componentes/buscador';
 import Cartas from './componentes/cartas';
-import Data from './componentes/data';
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 
 
 function App() {
+
   
-const [estado,setEstado] = useState(Data)
-const [temp,setTemp] = useState(Data)
+ let ojb={
+  "id": 30,
+  "name": "Rick Sanchez",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Male",
+  "origin": {
+    "name": "Earth (C-137)",
+    "url": "https://rickandmortyapi.com/api/location/1"
+  },
+  "location": {
+    "name": "Citadel of Ricks",
+    "url": "https://rickandmortyapi.com/api/location/3"
+  },
+  "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"} 
+const [estado,setEstado] = useState([])
+const [temp,setTemp] = useState([])
+const [agregando,setAgregando]=useState([])
 
 function dele (id){
  
@@ -16,15 +33,31 @@ setEstado(estado.filter((vi)=>vi.id!==id))//
 }
 
 const handleSearch = (nombre) => {
-  const nuevo = temp.filter(e => e.name.toLowerCase().includes(nombre.toLowerCase()) )
+  const nuevo = temp.filter (e => e.name.toLowerCase().includes(nombre.toLowerCase()) )
   setEstado(nuevo)
 }
 
+useEffect(()=>{
+  async function personajes (){
+    const persona= await fetch('https://rickandmortyapi.com/api/character')
+    const enconperson= await persona.json()
+    setEstado(enconperson.results)
+    setTemp(enconperson.results)
+  }
+  personajes()
+}
+  
+  ,[])
+
+  
+
   return (
     <div className="App">
+
       <Buscador handleSearch={handleSearch} />
+      <button onClick={()=>setEstado([{...ojb,id:Math.floor((Math.random() * 100)-2)},...estado])}> AGREGAR </button>
+
      <div className='contenedor-de-cartas' >
-      
       {estado.map(
         (persona)=>{
           return(
